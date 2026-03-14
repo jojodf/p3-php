480e085f0e6603b63c470ec4ef59fd11d0f7d139
WALEED <waleedalabed46@gmail.com>
2026-03-14 17:20:09 +0100

    opdracht db

--- hoofdopdracht/Project Structure/includes/header.php
+++ hoofdopdracht/Project Structure/includes/header.php
@@ -2,7 +2,7 @@
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
-    <title>Mijn Tracker</title>
+    <title>Mijn Notities</title>
 </head>
 <body>
-    <h1>Mijn Persoonlijke Tracker</h1>
+    <h1>Mijn Persoonlijke Notities</h1>
