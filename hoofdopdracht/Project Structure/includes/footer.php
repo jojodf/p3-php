795d66da8dd808d5393e1cf3dec60b85c89a687f
WALEED <waleedalabed46@gmail.com>
2026-03-21 13:26:29 +0100

    op

--- hoofdopdracht/Project Structure/includes/footer.php
+++ hoofdopdracht/Project Structure/includes/footer.php
@@ -4,5 +4,9 @@
     &copy; <?php echo date("Y")  ;?> 
 </footer>
 
+
+<p id="dbTime">Database geladen om: --:--</p>
+
+<script src="/Project Structure/js/app.js"></script>
 </body>
 </html>
--- hoofdopdracht/Project Structure/pages/toevoegen.php
+++ hoofdopdracht/Project Structure/pages/toevoegen.php
@@ -1,10 +1,29 @@
 <?php include "../includes/header.php"; ?>
 <?php include "../includes/nav.php"; ?>
+ 
 
-<h1>Toevoegen</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nieuw item toevoegen</title>
+</head>
+<body>
+    <form  method="POST" action="/Project Structure/pages/verwerk.php">
+
+    <LAbel for="titel"> Titel:</LAbel> <br>
+    <input type="text" id="titel" name="titel" required > <br> <br>
 
 
-<p>
-Hier komt later een formulier om een geams  toe te voegen.
-</p>
+     <label for="categorie">Categorie:</label><br>
+    <input type="text" id="categorie" name="categorie"><br><br>
+    <button type="submit">Toevoegen</button>
 
+
+
+
+</form>
+</body>
+</html>
