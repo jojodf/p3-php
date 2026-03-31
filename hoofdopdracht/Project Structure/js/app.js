e1721b634cc7258f7c9c4f7217d7925f12348535
WALEED <waleedalabed46@gmail.com>
2026-03-31 13:48:57 +0200

    fo

--- hoofdopdracht/Project Structure/js/app.js
+++ hoofdopdracht/Project Structure/js/app.js
@@ -8,3 +8,20 @@ if(minutes<10){
 }
 
 document.getElementById("dbTime").textContent ="Database geladen om:"+hours+":"+minutes;
+
+
+const footer = document.getElementById("funFooter");
+
+let clickTeller = 0;
+
+if (footer) {
+    footer.addEventListener("click", function() {
+        
+       
+        if (clickTeller === 5) {
+            alert("⚠️ Druk hier niet, sukkel! 😎😂 ");
+            
+            clickTeller = 0; 
+        }
+    });
+}
