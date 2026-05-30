0e6d63429eba7b6fb90dcc7c29fa8dc32d38fe86
WALEED <waleedalabed46@gmail.com>
2026-05-30 17:17:24 +0200

    update

--- hoofdopdracht/Project Structure/BAGES/edit.php
+++ hoofdopdracht/Project Structure/BAGES/edit.php
@@ -2,15 +2,21 @@
 
 include "../INCLUDES/db.php";
 
+if (!isset($_GET['id'])) {
+    die("Geen ID opgegeven.");
+}
+
 $id = $_GET['id'];
 
 $sql = "SELECT * FROM items WHERE id = ?";
-
 $stmt = $conn->prepare($sql);
-
 $stmt->execute([$id]);
 
 $item = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$item) {
+    die("Item niet gevonden.");
+}
 
 ?>
 
